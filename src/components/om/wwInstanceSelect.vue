c6a3cdbc74bf4e9dbbdaf35ee73f4690d5304004
王伟 <ERVeepp@users.noreply.github.com>
2020-11-18 14:22:58 +0800

    重写级联获取Instance Options
    2020年11月18日14:22:56
    王伟

---
+++ src/components/om/wwInstanceSelect.vue
@@ -0,0 +1,166 @@
+<!-- Instance select component the props defaultValue is the default checked  -->
+<template>
+  <a-form-item laout="inline">
+    <a-select
+      showSearch
+      :style="{ width: width }"
+      placeholder="Instance"
+      size="default"
+      :allowClear="allowClear"
+      :disabled="disabled"
+      :mode="mode"
+      :showArrow="!allowClear"
+      optionLabelProp="title"
+      :dropdownMatchSelectWidth="false"
+      v-decorator="[modelName, {initialValue: initValue}]"
+      :filterOption="instanceFilterOption"
+      @change="handleChange">
+      <a-select-option v-for="instance in data" :key="instance.id" :title="instance.name">
+        <div style="display: inline-block;vertical-align: middle;">
+          <div style="color:#333333;font-size: 14px;">
+            <ellipsis :length="30">{{ instance.name }}</ellipsis>
+          </div>
+          <div style="color:#999999;font-size: 12px;">
+            <ellipsis :length="50">{{ instance.placementKey }}</ellipsis>
+          </div>
+        </div>
+        <p style="display:none">{{ instance.placementKey + ' ' + instance.name }}</p>
+      </a-select-option>
+    </a-select>
+  </a-form-item>
+</template>
+
+<script>
+import { Ellipsis } from '../../components'
+import { instancesSelectList } from '../../api/mediation'
+import { mapState } from 'vuex'
+
+export default {
+  name: 'OmInstanceSelect',
+  components: {
+    Ellipsis
+  },
+  props: {
+    modelName: {
+      type: String,
+      default: 'instanceId'
+    },
+    width: {
+      type: String,
+      default: '220px'
+    },
+    mode: {
+      type: String,
+      default: 'default'
+    },
+    allowClear: {
+      type: Boolean,
+      default: true
+    },
+    defaultValue: {
+      type: Array,
+      default () {
+        return []
+      }
+    },
+    placementId: {
+      type: Array,
+      default: null
+    },
+    adnAppId: {
+      type: Array,
+      default: null
+    },
+    disabled: {
+      type: Boolean,
+      default: false
+    },
+    adnIds: {
+      type: Array,
+      default: null
+    }
+  },
+  data () {
+    return {
+      data: [],
+      initValue: this.defaultValue
+    }
+  },
+  computed: mapState({
+    searchPlacement: state => state.publisher.searchPlacement
+  }),
+  methods: {
+    handleChange (value, option) {
+      this.$emit('change', value)
+      this.$emit('instanceSelecteChange', value)
+    },
+    instanceFilterOption (input, option) {
+      return (option.componentOptions.children[1].children[0].text.toLowerCase().indexOf(input.toLowerCase()) >= 0)
+    },
+    setOptions () {
+      console.log('setOptions')
+      const { searchApp: pubAppId } = this.$store.state.publisher
+      if (!this.data || this.data.length === 0) {
+        const params = { pubAppId, placementId: this.placementId }
+        params.adNetworkAppId = this.adnAppId.join(',')
+        params.adnIds = this.adnIds.join(',')
+        params.placementId = this.placementId.join(',')
+        params.adNetworkIds = this.adnIds.join(',')
+        instancesSelectList(params).then(res => {
+          if (res.code === 0) {
+            this.data = res.data
+          }
+        })
+      }
+    },
+    resetOptions () {
+      console.log('resetOptions')
+      const { searchApp: pubAppId } = this.$store.state.publisher
+      const params = { pubAppId, placementId: this.placementId }
+      params.adNetworkAppId = this.adnAppId.join(',')
+      params.adnIds = this.adnIds.join(',')
+      params.placementId = this.placementId.join(',')
+      params.adNetworkIds = this.adnIds.join(',')
+      instancesSelectList(params).then(res => {
+        if (res.code === 0) {
+          this.data = res.data
+        }
+      })
+    },
+    upload () {
+      console.log('upload')
+      const { searchApp: pubAppId } = this.$store.state.publisher
+      const params = { pubAppId, placementId: this.placementId }
+      params.adNetworkAppId = this.adnAppId.join(',')
+      params.adnIds = this.adnIds.join(',')
+      params.placementId = this.placementId.join(',')
+      params.adNetworkIds = this.adnIds.join(',')
+      instancesSelectList(params).then(res => {
+        if (res.code === 0) {
+          this.data = res.data
+        }
+      })
+    }
+  },
+  created () {
+    this.setOptions()
+  },
+  watch: {
+    adnAppId (val) {
+      this.resetOptions()
+    },
+    adnIds (val) {
+      this.resetOptions()
+    },
+    placementId (val) {
+      this.resetOptions()
+    },
+    searchPlacement (val) {
+      this.resetOptions()
+    }
+  }
+}
+</script>
+
+<style type="less" scoped>
+</style>
